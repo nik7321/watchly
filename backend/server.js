88294d074a2d9e7a886c8372f52ab1ee536b172f
Nikhil <npathania502@gmail.com>
2024-11-19 18:42:41 +0530

    fixed space

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,5 @@ app.use("/api/v1/tv",protectRoute,tvRoutes);
 app.use("/api/v1/search",protectRoute,searchRoutes);
 
 app.listen(PORT,() => {
-    console.log("Server started at http://localhost:"+ PORT); 
-  
+    console.log("Server started at http://localhost:"+ PORT);
 })
